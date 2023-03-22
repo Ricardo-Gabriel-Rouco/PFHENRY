eb7bbd8f3c60d03f98eed1fc33706e7a01f997aa
mmitacc <manuelmitacc@hotmail.com>
2023-03-21 22:47:29 -0500

    add(css reponsive):Card, CarContainer, index

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,16 +1,20 @@
+import style from "./Card.module.css";
+import img_book from "../../image/book.jpg";
 
 const Card = (props) => {
-    
-    
-        return (
-        <div>
-            <h1>{props.title}</h1>
-            <h2>Just imagine there is a wonderful image of a wonderful book</h2>
-            <p>Stock: {props.stock}</p>
-            <p>Price: {props.price}</p>
-        </div>
-
-    );
-}
+  return (
+    <div className={style.card}>
+      <img
+        src={props.image ? props.image : img_book}
+        alt={props.title}
+        className={style.image}
+      />
+      <h3>{props.title}</h3>
+      <h5>Just imagine there is a wonderful image of a wonderful book</h5>
+      <h5 className={style.floatingRight}>Stock: {props.stock}</h5>
+      <h5 className={style.floatingLeft}>Price: {props.price}</h5>
+    </div>
+  );
+};
 
 export default Card;
