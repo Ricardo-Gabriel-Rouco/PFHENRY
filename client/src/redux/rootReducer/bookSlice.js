8c46a2e0e6fac61eb11e225a6d706987c3075a84
Diego Hernan Piola <diegohernanpiola@gmail.com>
2023-03-24 19:00:16 -0300

    cree action search, combiene filtros, se puede filtarar por autor y despues genero y vceversa

--- client/src/redux/rootReducer/bookSlice.js
+++ client/src/redux/rootReducer/bookSlice.js
@@ -20,25 +20,75 @@ export const booksSlice = createSlice({
       state.booksToFilter = alfa;
     },
 
+    searchBook: (state, action) => {
+      const search = state.allBooks;
+      state.booksToFilter =
+        action.payload === ""
+          ? search
+          : search.filter((book) => book.title.toLowerCase() === action.payload.toLowerCase());
+    },
+
     filterByAuthor: (state, action) => {
       const allAuthors = state.allBooks;
-      state.booksToFilter =
-        action.payload === "all"
-          ? allAuthors
-          : allAuthors.filter((auth) => auth.author === action.payload);
+      if (state.allBooks.length === state.booksToFilter.length) {
+        state.booksToFilter =
+          action.payload === "all"
+            ? allAuthors
+            : allAuthors.filter((auth) => auth.author === action.payload);
+      }
+      let genre;
+      state.booksToFilter.find((books) => {
+        genre = books.genre;
+        return books.genre;
+      });
+      if (state.booksToFilter.some((elem) => elem.author === action.payload)) {
+        state.booksToFilter =
+          action.payload === "all"
+            ? allAuthors
+            : state.booksToFilter.filter((auth) => auth.author === action.payload);
+      } else {
+        state.booksToFilter = allAuthors;
+        state.booksToFilter =
+          action.payload === "all"
+            ? allAuthors
+            : state.booksToFilter.filter(
+                (elem) => elem.genre === genre && elem.author === action.payload
+              );
+      }
     },
 
     filterByGenre: (state, action) => {
       const allGenres = state.allBooks;
-      allGenres.filter((elem) => elem.genre === action.payload);
-      state.booksToFilter =
-        action.payload === "all"
-          ? allGenres
-          : allGenres.filter((elem) => elem.genre === action.payload);
+      if (state.allBooks.length === state.booksToFilter.length) {
+        allGenres.filter((elem) => elem.genre === action.payload);
+        state.booksToFilter =
+          action.payload === "all"
+            ? allGenres
+            : allGenres.filter((elem) => elem.genre === action.payload);
+      }
+      let author;
+      state.booksToFilter.find((books) => {
+        author = books.author;
+        return books.genre;
+      });
+      if (state.booksToFilter.some((elem) => elem.genre === action.payload)) {
+        state.booksToFilter =
+          action.payload === "all"
+            ? allGenres
+            : state.booksToFilter.filter((auth) => auth.genre === action.payload);
+      } else {
+        state.booksToFilter = allGenres;
+        state.booksToFilter =
+          action.payload === "all"
+            ? allGenres
+            : state.booksToFilter.filter(
+                (elem) => elem.author === author && elem.genre === action.payload
+              );
+      }
     },
   },
 });
 
-export const { addBook, filterByAuthor, filterByGenre } = booksSlice.actions;
+export const { addBook, searchBook, filterByAuthor, filterByGenre } = booksSlice.actions;
 
 export default booksSlice.reducer;
