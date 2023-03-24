0ee0707d9186cabdbe0b02d4ecef998c220fea45
Gabriel Rouco <r.g.rouco1@gmail.com>
2023-03-24 20:34:31 -0300

    funcion crud editorial

---
+++ client/src/firebase/firestore/editorial.js
@@ -0,0 +1,71 @@
+import { getDocs, query, collection, where, doc, getDoc, updateDoc, addDoc } from "firebase/firestore"
+import {db} from '../firebase-config'
+
+export async function getEditors () {
+
+  const q = query(collection(db, "editorial"), where('display', '==', true))
+  const querySnapshot = await getDocs(q);
+  let data = [];
+  querySnapshot.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    data.push({
+      ...doc.data(),
+      id: doc.id
+    }
+    )
+  })
+  return data
+}
+
+
+export async function getEditorById (id) {
+  try {
+    const docsRef = doc(db, 'editorial', id)
+    const docSnap = await getDoc(docsRef)
+    if (docSnap.exists()) {
+        return {...docSnap.data(), id: id};
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export async function deleteEditor (id){
+  try {
+    const docsRef = doc(db, 'editorial', id)
+    await updateDoc(docsRef, {
+      display: false
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export async function postEditor (name){
+  // validaciones, muchas validaciones
+
+  // fin validaciones
+  try {
+    const newAuthor = doc(db, 'editorial')
+    await addDoc(newAuthor, {
+      name: name,
+      display: true
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+// despues voy a revisar esta funcion, por favor usarla con precaucion
+export async function modifyEditor (name, id){
+  try {
+    const newBook = doc(db, 'editorial', id)
+    await updateDoc(newBook, {
+      name: name,
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
