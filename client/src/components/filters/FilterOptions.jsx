22a7247055d4ad9dcb5ed97bbb258c12dbd6ddc2
Diego Hernan Piola <diegohernanpiola@gmail.com>
2023-03-25 18:58:09 -0300

    Diego: Redux filtros y ordenamiento

--- client/src/components/filters/FilterOptions.jsx
+++ client/src/components/filters/FilterOptions.jsx
@@ -1,9 +1,11 @@
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
-import { filterByAuthor, filterByGenre } from "../../redux/rootReducer/bookSlice";
+import { filterBooks, orderByRating, removeFilter, reset } from "../../redux/rootReducer/bookSlice";
 
 export const FilterOptions = ({ setCurrentPage }) => {
   const booksList = useSelector((state) => state.books.allBooks);
+
+  const fliters = useSelector((state) => state.books.filtersApplied);
 
   const dispatch = useDispatch();
 
@@ -14,43 +16,90 @@ export const FilterOptions = ({ setCurrentPage }) => {
 
   const handlerAuthor = (e) => {
     e.preventDefault();
-    dispatch(filterByAuthor(e.target.value));
+    dispatch(filterBooks(["author", e.target.value]));
     setCurrentPage(1);
   };
 
   const handlerGenre = (e) => {
     e.preventDefault();
-    dispatch(filterByGenre(e.target.value));
+    dispatch(filterBooks(["genre", e.target.value]));
+    setCurrentPage(1);
+  };
+
+  const handlerRating = (e) => {
+    e.preventDefault();
+    dispatch(orderByRating(e.target.value));
+    setCurrentPage(1);
+  };
+
+  const handlerReset = (e) => {
+    e.preventDefault();
+    dispatch(reset());
+    setCurrentPage(1);
+  };
+
+  const handlerRemoveFilter = (e) => {
+    e.preventDefault();
+    dispatch(removeFilter(e.target.id));
     setCurrentPage(1);
   };
 
   return (
     <div>
-      <select>
-        <option value="all" onClick={handlerAuthor}>
-          All Authors
-        </option>
-        {booksList.length &&
-          formatArray(booksList.map((elem) => elem.author).sort()).map((elem, index) =>
-            elem !== null ? (
-              <option key={index} value={elem} onClick={handlerAuthor}>
-                {elem}
-              </option>
-            ) : null
-          )}
-      </select>
+      <div>
+        <select>
+          <option value="all" onClick={handlerAuthor}>
+            All Authors
+          </option>
+          {booksList.length &&
+            formatArray(booksList.map((elem) => elem.author).sort()).map((elem, index) =>
+              elem !== null ? (
+                <option key={index} value={elem} onClick={handlerAuthor}>
+                  {elem}
+                </option>
+              ) : null
+            )}
+        </select>
 
-      <select>
-        <option value="all" onClick={handlerGenre}>
-          All Genres
-        </option>
-        {booksList.length &&
-          formatArray(booksList.map((elem) => elem.genre).sort()).map((gen, index) => (
-            <option key={index} value={gen} onClick={handlerGenre}>
-              {gen}
-            </option>
+        <select>
+          <option value="all" onClick={handlerGenre}>
+            All Genres
+          </option>
+          {booksList.length &&
+            formatArray(booksList.map((elem) => elem.genre).sort()).map((gen, index) => (
+              <option key={index} value={gen} onClick={handlerGenre}>
+                {gen}
+              </option>
+            ))}
+        </select>
+
+        <select>
+          <option value="min" onClick={handlerRating}>
+            Min Rating
+          </option>
+          <option value="max" onClick={handlerRating}>
+            Max Rating
+          </option>
+        </select>
+
+        <select>
+          <option value="reset" onClick={handlerReset}>
+            Reset
+          </option>
+        </select>
+      </div>
+
+      <dvi>
+        {fliters &&
+          fliters.map((filter, index) => (
+            <div key={index} id={index}>
+              <button id={index} onClick={handlerRemoveFilter}>
+                X
+              </button>
+              <p>{filter[1]}</p>
+            </div>
           ))}
-      </select>
+      </dvi>
     </div>
   );
 };
