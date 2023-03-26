12ec39d0bc8be71dff2937d88d22974f14464716
Gabriel Rouco <r.g.rouco1@gmail.com>
2023-03-26 17:53:07 -0300

    elementos html bookform

--- client/src/components/BookForm/BookForm.jsx
+++ client/src/components/BookForm/BookForm.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function BookForm() {
+  
   return (
     <div>
       <form action="">
-        
+        <label htmlFor="">Titulo: </label>
+        <input type="text" placeholder='Ej: La llamada de Cthulhu' />
+        <label htmlFor="">Autor: </label>
+        <input type="text" placeholder='Ej: H. P. Lovecraft'/>
+        <label htmlFor="">Editorial: </label>
+        <input type="text" placeholder='Ej: Gargola'/>
+        <label htmlFor="">Generos: </label>
+        <input type="text" placeholder='Selecciona uno o varios de la lista'/>
+        <label htmlFor="">Imagen: </label>
+        <input type="text" placeholder='Sube un archivo o pega un link'/>
+        <label htmlFor="" >Precio</label>
+        <input type="text" placeholder='Ingresa el precio'/>
+        <label htmlFor="">Año de publiacion</label>
+        <input type="text" placeholder='Ej: 1926'/>
+        <button type='submit'>Guardar</button>
       </form>
     </div>
   )
