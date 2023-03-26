3a3acd9c337a346c97535788a661bbde2ab44f59
Gabriel Rouco <r.g.rouco1@gmail.com>
2023-03-26 18:05:51 -0300

    agregado campos y estado de control de imagen

--- client/src/components/BookForm/BookForm.jsx
+++ client/src/components/BookForm/BookForm.jsx
@@ -1,23 +1,61 @@
 import React, {useState} from 'react'
 
 function BookForm() {
-  
+  // estado que maneja la subida de imagen
+  const [image, setImage] = useState('');
+  const handleImageInputChange = (ev)=>{
+    //Esta parte solo sirve para mostrar la imagen
+    const reader = new FileReader()
+    reader.readAsDataURL(ev.target.files[0]);
+    reader.onloadend = () => {
+    setImage(reader.result)
+    };
+  }
+
+  const [bookData, setbookData] = useState({
+    title: '',
+    author: '',
+    editorial: '',
+    genres: [],
+    price: '',
+    year: ''
+
+  })
+
+  const onClose = () => {
+    setImage('')
+  }
+
   return (
     <div>
       <form action="">
-        <label htmlFor="">Titulo: </label>
+        <label htmlFor="title">Titulo: </label>
         <input type="text" placeholder='Ej: La llamada de Cthulhu' />
-        <label htmlFor="">Autor: </label>
+        <label htmlFor="author">Autor: </label>
         <input type="text" placeholder='Ej: H. P. Lovecraft'/>
-        <label htmlFor="">Editorial: </label>
+        <label htmlFor="editorial">Editorial: </label>
         <input type="text" placeholder='Ej: Gargola'/>
-        <label htmlFor="">Generos: </label>
+        <label htmlFor="genres">Generos: </label>
         <input type="text" placeholder='Selecciona uno o varios de la lista'/>
-        <label htmlFor="">Imagen: </label>
-        <input type="text" placeholder='Sube un archivo o pega un link'/>
-        <label htmlFor="" >Precio</label>
+        <label htmlFor="image">Imagen: </label>
+        {!image?
+        <input 
+        type='file'
+        accept='image/*'
+        name="image" 
+        placeholder="Select an image" 
+        onChange={handleImageInputChange}/>
+        
+        :<div >
+          <img  src={image} alt='uploaded_Image'/>
+          <div >
+            <button onClick={onClose}>X</button>
+          </div>
+        </div>
+        }
+        <label htmlFor="price" >Precio</label>
         <input type="text" placeholder='Ingresa el precio'/>
-        <label htmlFor="">Año de publiacion</label>
+        <label htmlFor="year">Año de publiacion</label>
         <input type="text" placeholder='Ej: 1926'/>
         <button type='submit'>Guardar</button>
       </form>
@@ -26,3 +64,10 @@ function BookForm() {
 }
 
 export default BookForm
+
+    // Esto es para subir al Storage
+    // vamos a usar un dispatch para esa accion
+    // const imagesRef = ref(storage, 'test1.jpeg');
+    //   uploadBytes(imagesRef, ev.target.files[0],{contentType: 'image/jpeg',}).then((snapshot) => {
+    //   console.log(snapshot);
+    // });
