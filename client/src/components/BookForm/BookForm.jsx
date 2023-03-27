737b06013b39e22d843bc6dcf014ef264c7f9baf
Gabriel Rouco <r.g.rouco1@gmail.com>
2023-03-27 14:18:07 -0300

    un poco mas de estilado

--- client/src/components/BookForm/BookForm.jsx
+++ client/src/components/BookForm/BookForm.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import styles from "./BookForm.module.css";
 import { getGenres } from "../../firebase/firestore/genres";
-import validation from './validation'
-import {Input, InputLabel, Button} from '@mui/material'
-
+import validation from "./validation";
+import { Input, InputLabel, Button, Box, Container} from "@mui/material";
+import SaveIcon from "@mui/icons-material/Save";
+import ErrorIcon from "@mui/icons-material/Error";
 function BookForm() {
   // estado que maneja la subida de imagen
   const [imageFile, setImageFile] = useState("");
@@ -45,11 +46,11 @@ function BookForm() {
     title: "",
     author: "",
     editorial: "",
-    genres: [],
+    genres: "",
     price: "",
     year: "",
-    image: ''
-  })
+    image: "",
+  });
 
   const onClose = () => {
     setImageFile("");
@@ -61,7 +62,7 @@ function BookForm() {
       setbookData({ ...bookData, image: { link: e.target.value } });
     }
     setbookData({ ...bookData, [e.target.name]: e.target.value });
-    setErrors(validation(bookData))
+    setErrors(validation(bookData));
   };
 
   const handleOptions = (e) => {
@@ -93,122 +94,161 @@ function BookForm() {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit} className={styles.formContainer}>
-        <InputLabel htmlFor="isbn">ISBN: </InputLabel>
-        <Input
-          type="text"
-          name="isbn"
-          placeholder="Ej: 9788415618683"
-          onChange={handleInputChange}
-          value={bookData.isbn}
-        />
-        {errors.isbn? <p>{errors.isbn}</p>: null}
-        <InputLabel htmlFor="title">Titulo: </InputLabel>
-        <Input
-          type="text"
-          name="title"
-          placeholder="Ej: La llamada de Cthulhu"
-          onChange={handleInputChange}
-          value={bookData.title}
-        />
-        {errors.title? <p>{errors.title}</p>: null}
-        <InputLabel htmlFor="author">Autor: </InputLabel>
-        <Input
-          type="text"
-          name="author"
-          placeholder="Ej: H. P. Lovecraft"
-          onChange={handleInputChange}
-          value={bookData.author}
-        />
-        {errors.author? <p>{errors.author}</p>: null}
-        <InputLabel htmlFor="editorial">Editorial: </InputLabel>
-        <Input
-          type="text"
-          name="editorial"
-          placeholder="Ej: Alma ediciones"
-          onChange={handleInputChange}
-          value={bookData.editorial}
-        />
-        {errors.editorial? <p>{errors.editorial}</p>: null}
-        <InputLabel htmlFor="genres">Generos:</InputLabel>
-        <select
-          multiple={true}
-          value={bookData.genres}
-          onChange={handleOptions}
-        >
-          {genres
-            ?.sort((a, b) => a.name.localeCompare(b.name))
-            .map((genre) => (
-              <option key={genre.name} value={genre.name}>
-                {genre.name}
-              </option>
-            ))}
-        </select>
-        <div>
-          {bookData.genres.map(
-            (genre) =>
-              bookData.genres.indexOf(genre) ===
-                bookData.genres.lastIndexOf(genre) && (
-                <div key={genre}>
-                  {genre}
-                  <button onClick={() => handleRemove(genre)}>X</button>
-                </div>
-              )
-          )}
-        </div>
-        {errors.genres? <p>{errors.genres}</p>: null}
-        <InputLabel htmlFor="image">Imagen: </InputLabel>
-        {!imageFile ? (
+    <Container>
+      <Box>
+        <form onSubmit={handleSubmit}>
+          <InputLabel htmlFor="isbn">ISBN: </InputLabel>
+          <Input
+            type="text"
+            name="isbn"
+            placeholder="Ej: 9788415618683"
+            onChange={handleInputChange}
+            value={bookData.isbn}
+          />
+          {errors.isbn ? (
+            <p className={styles.formError}>
+              <ErrorIcon />
+              {errors.isbn}
+            </p>
+          ) : null}
+          <InputLabel htmlFor="title">Titulo: </InputLabel>
+          <Input
+            type="text"
+            name="title"
+            placeholder="Ej: La llamada de Cthulhu"
+            onChange={handleInputChange}
+            value={bookData.title}
+          />
+          {errors.title ? (
+            <p className={styles.formError}>
+              <ErrorIcon />
+              {errors.title}
+            </p>
+          ) : null}
+          <InputLabel htmlFor="author">Autor: </InputLabel>
+          <Input
+            type="text"
+            name="author"
+            placeholder="Ej: H. P. Lovecraft"
+            onChange={handleInputChange}
+            value={bookData.author}
+          />
+          {errors.author ? (
+            <p className={styles.formError}>
+              <ErrorIcon />
+              {errors.author}
+            </p>
+          ) : null}
+          <InputLabel htmlFor="editorial">Editorial: </InputLabel>
+          <Input
+            type="text"
+            name="editorial"
+            placeholder="Ej: Alma ediciones"
+            onChange={handleInputChange}
+            value={bookData.editorial}
+          />
+          {errors.editorial ? (
+            <p className={styles.formError}>
+              <ErrorIcon />
+              {errors.editorial}
+            </p>
+          ) : null}
+          <InputLabel htmlFor="genres">Generos:</InputLabel>
+          <select
+            multiple={true}
+            value={bookData.genres}
+            onChange={handleOptions}
+          >
+            {genres
+              ?.sort((a, b) => a.name.localeCompare(b.name))
+              .map((genre) => (
+                <option key={genre.name} value={genre.name}>
+                  {genre.name}
+                </option>
+              ))}
+          </select>
           <div>
-            <Input
-              type="file"
-              accept="image/*"
-              name="imageFile"
-              placeholder="Select an image"
-              onChange={handleImageInputChange}
-            />
-            <InputLabel htmlFor="imageLink">Url Imagen:</InputLabel>
-            <Input
-              type="text"
-              name="imageLink"
-              value={bookData.image.link}
-              onChange={handleInputChange}
-            />
+            {bookData.genres.map(
+              (genre) =>
+                bookData.genres.indexOf(genre) ===
+                  bookData.genres.lastIndexOf(genre) && (
+                  <div key={genre}>
+                    {genre}
+                    <button onClick={() => handleRemove(genre)}>X</button>
+                  </div>
+                )
+            )}
           </div>
-        ) : (
-          <div>
-            {bookData.image.file ? (
-              <img src={bookData.image.file} alt="uploaded_Image" />
-            ) : bookData.image.link ? (
-              <img src={bookData.image.link} alt="uploaded_Image" />
-            ) : null}
+          {errors.genres ? (
+            <p className={styles.formError}>
+              <ErrorIcon />
+              {errors.genres}
+            </p>
+          ) : null}
+          <InputLabel htmlFor="image">Imagen: </InputLabel>
+          {!imageFile ? (
             <div>
-              <button onClick={onClose}>X</button>
+              <Input
+                type="file"
+                accept="image/*"
+                name="imageFile"
+                placeholder="Select an image"
+                onChange={handleImageInputChange}
+              />
+              <InputLabel htmlFor="imageLink">Url Imagen:</InputLabel>
+              <Input
+                type="text"
+                name="imageLink"
+                value={bookData.image.link}
+                onChange={handleInputChange}
+              />
             </div>
-          </div>
-        )}
-        {errors.image? <p>{errors.image}</p>: null}
-        <InputLabel htmlFor="price">Precio</InputLabel>
-        <Input
-          type="text"
-          name="price"
-          placeholder="Ingresa el precio"
-          onChange={handleInputChange}
-          value={bookData.price}
-        />
-        {errors.price? <p>{errors.price}</p>: null}
-        <InputLabel htmlFor="year">Año de publiacion</InputLabel>
-        <Input
-          type="text"
-          name="year"
-          placeholder="Ej: 1926"
-          onChange={handleInputChange}
-          value={bookData.year}
-        />
-        <Button type="submit">Guardar</Button>
-      </form>
-    </div>
+          ) : (
+            <div>
+              {bookData.image.file ? (
+                <img src={bookData.image.file} alt="uploaded_Image" />
+              ) : bookData.image.link ? (
+                <img src={bookData.image.link} alt="uploaded_Image" />
+              ) : null}
+              <div>
+                <button onClick={onClose}>X</button>
+              </div>
+            </div>
+          )}
+          {errors.image ? (
+            <p className={styles.formError}>
+              <ErrorIcon />
+              {errors.image}
+            </p>
+          ) : null}
+          <InputLabel htmlFor="price">Precio</InputLabel>
+          <Input
+            type="text"
+            name="price"
+            placeholder="Ingresa el precio"
+            onChange={handleInputChange}
+            value={bookData.price}
+          />
+          {errors.price ? (
+            <p className={styles.formError}>
+              <ErrorIcon />
+              {errors.price}
+            </p>
+          ) : null}
+          <InputLabel htmlFor="year">Año de publiacion</InputLabel>
+          <Input
+            type="text"
+            name="year"
+            placeholder="Ej: 1926"
+            onChange={handleInputChange}
+            value={bookData.year}
+          />
+          <Button type="submit">
+            <SaveIcon /> Guardar
+          </Button>
+        </form>
+      </Box>
+    </Container>
   );
 }
 
