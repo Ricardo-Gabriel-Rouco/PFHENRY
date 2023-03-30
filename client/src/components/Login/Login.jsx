a615d71c770f2f4819fe5ef3cf2ad2c6cd3533a5
Gabriel Rouco <r.g.rouco1@gmail.com>
2023-03-30 20:31:51 -0300

    refactorizado login con mail

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -4,8 +4,8 @@ import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/authContext";
 
 const Login = () => {
-  const {login} = useAuth()
-  const navigate = useNavigate()
+  const { login } = useAuth();
+  const navigate = useNavigate();
   const [userData, setUserData] = useState({
     email: "",
     password: "",
@@ -16,21 +16,22 @@ const Login = () => {
     password: "",
   });
 
-  async function handleSubmit() {
+  async function handleSubmit(e) {
+    e.preventDefault()
     try {
-      await login(userData.email, userData.password)
-      navigate('/home')
+      await login(userData.email, userData.password);
+      navigate("/home");
     } catch (error) {
-      console.log(error.code)
+      if(error.code === 'auth/wrong-password')  setErrors({...errors, password: 'Wrong password'})
+      if(error.code === 'auth/user-not-found')  setErrors({...errors, email: 'User not found'})
     }
   }
 
   async function registerGoogle() {
     try {
-    
-      navigate('/home')
+      navigate("/home");
     } catch (error) {
-      alert('Error al iniciar sesion: ', error)
+      setErrors({...errors, error})
     }
   }
 
@@ -39,13 +40,52 @@ const Login = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit} style={{ display: "flex", flexDirection: "column", alignItems: "center", margin: "2rem" }}>
-      <TextField type="text" name="email" value={userData.email} label="Correo electrónico" onChange={handleInputChange} style={{ margin: "1rem" }}/>
-      <TextField type="password" name="password" value={userData.password} label="Contraseña" onChange={handleInputChange} style={{ margin: "1rem" }}/>
-      <Button type="submit" variant="contained" color="primary" style={{ margin: "2rem" }}>Iniciar Sesion</Button>
-        <Button variant="contained" color="secondary" onClick={() => {registerGoogle();}} style={{ margin: "2rem" }}>
-          Inicia sesion con Google
-        </Button>
+    <form
+      onSubmit={handleSubmit}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        margin: "2rem",
+      }}
+    >
+      <TextField
+        type="text"
+        name="email"
+        value={userData.email}
+        label="Correo electrónico"
+        onChange={handleInputChange}
+        style={{ margin: "1rem" }}
+      />
+      {errors.email && <p>{errors.email}</p>}
+
+      <TextField
+        type="password"
+        name="password"
+        value={userData.password}
+        label="Contraseña"
+        onChange={handleInputChange}
+        style={{ margin: "1rem" }}
+      />
+      {errors.password && <p>{errors.password}</p>}
+      <Button
+        type="submit"
+        variant="contained"
+        color="primary"
+        style={{ margin: "2rem" }}
+      >
+        Iniciar Sesion
+      </Button>
+      <Button
+        variant="contained"
+        color="secondary"
+        onClick={() => {
+          registerGoogle();
+        }}
+        style={{ margin: "2rem" }}
+      >
+        Inicia sesion con Google
+      </Button>
       <Link to={"/register"}>No tienes Cuenta? crea una</Link>
     </form>
   );
