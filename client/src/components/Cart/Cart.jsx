0e5a054f43a554b813c3b38346d0f2e9d24d109f
Joaquin Santiago Oliveira <104986417+fr0st1987@users.noreply.github.com>
2023-04-03 15:57:01 -0300

    se agrega post al back con el body para la pasarela de pago

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -7,15 +7,47 @@ import { Stack, Box } from '@mui/material';
 import { Divider, IconButton, Grid } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 import { toogleCart } from '../../redux/rootReducer/toogleSlice';
+import { useState, useEffect } from 'react';
+import axios from 'axios'
+
 
 const Cart = () => {
     const dispatch = useDispatch();
     const cart = useSelector(state => state.cart);
     const abrir = useSelector(state => state.toogle.isOpen)
+    const [order, setOrder] = useState({})
 
-    console.log(cart.cart.totalPrice)
+    useEffect(() => {
+        setOrder({
+            user: {
+                id: 1234,
+                name: "prueba",
+                email: "prueba@gmail.com",
+            },
+            items: cart.cart.cart.map((product) => ({
+                id: product.id,
+                title: product.title,
+                unit_price: product.price,
+                quantity: product.quantity,
+            })),
+        });
+    }, [cart.cart.cart]);
+
+    console.log(order);
 
     //handlers
+
+    const handleBuy = async () => {
+        try {
+            const response = await axios.post('http://localhost:3001/checkout', order);
+            console.log("Response:", response);
+            dispatch(removeAllProducts())
+            // Hacer algo con la respuesta exitosa
+        } catch (error) {
+            console.error("Error:", error.response.data);
+            // Manejar el error
+        }
+    }
 
     const handleClose = () => {
         dispatch(toogleCart())
@@ -38,7 +70,7 @@ const Cart = () => {
             <Box sx={{ width: '25em', p: 2 }}>
                 <Stack direction="row" justifyContent="space-between" alignItems="center">
                     <Typography variant="h5">Cart</Typography>
-                    <Button variant= 'contained' color="primary" size="small" sx={{ ml: 1 }}onClick={() => handleClose()}>
+                    <Button variant='contained' color="primary" size="small" sx={{ ml: 1 }} onClick={() => handleClose()}>
                         <CloseIcon />
                     </Button>
                 </Stack>
@@ -85,16 +117,26 @@ const Cart = () => {
                 {cart.cart.cart.length === 0 && (
                     <Typography variant="subtitle1">There is no product in your cart</Typography>
                 )}
-                {cart.cart.cart.length !== 0 && <Typography variant="subtitle1">{'Total Price: $ ' + cart.cart.totalPrice}</Typography> }
+                {cart.cart.cart.length !== 0 && <Typography variant="subtitle1">{'Total Price: $ ' + cart.cart.totalPrice}</Typography>}
                 <Button
                     onClick={() => handleRemoveAll()}
                     variant="contained"
                     color="secondary"
                     size="small"
-                    sx={{ marginTop: 2 }}
+                    sx={{ marginTop: 2, marginRight: 2, }}
                 >
                     Remove all products
                 </Button>
+                <Button
+                    onClick={() => handleBuy()}
+                    variant="contained"
+                    color="secondary"
+                    size="small"
+                    sx={{ marginTop: 2 }}
+                >
+                    Buy Products
+                </Button>
+
             </Box>
         </Drawer>
     )
