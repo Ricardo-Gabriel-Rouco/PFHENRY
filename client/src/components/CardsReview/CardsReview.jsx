1626308d41e9761a82c0aee3ea8fd968c6e4ca4c
mmitacc <manuelmitacc@hotmail.com>
2023-04-06 14:13:44 -0500

    fix(CardReview.jsx; CardNewReview.jsx): reponsive

--- client/src/components/CardsReview/CardsReview.jsx
+++ client/src/components/CardsReview/CardsReview.jsx
@@ -1,53 +1,71 @@
 import {
   Typography,
-  ListItem,
   Divider,
   ListItemText,
-  ListItemAvatar,
   Avatar,
   Rating,
+  Grid,
+  List,
 } from "@mui/material";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 const CardsReview = ({ user, comment, rating }) => {
   return (
-    <>
-      <ListItem
-        alignItems="flex-end"
-        justifyContent="space-between"
-        sx={{
-          width: "100%",
-        }}
+    <List>
+      <Grid
+        container
+        spacing={1}
+        sx={{ display: "flex", alignItems: "center" }}
       >
-        <ListItemAvatar>
-          <Avatar alt="User">
-            <AccountCircleIcon />
+        <Grid
+          item
+          xs={12}
+          sm={2}
+          md={2}
+          lg={2}
+          xl={2}
+          sx={{ display: "flex", justifyContent: "center" }}
+        >
+          <Avatar alt="User" color="primary">
+            <AccountCircleIcon fontSize="large" color="secondary" />
           </Avatar>
-        </ListItemAvatar>
-        <ListItemText
-          primary={user}
-          secondary={
-            <>
-              <Typography
-                sx={{ display: "inline", width: "100%" }}
-                component="span"
-                variant="body2"
-                color="text.primary"
-              ></Typography>
-              {comment}
-            </>
-          }
-        />
-        <Rating
-          name="read-only"
-          value={rating}
-          size="small"
-          precision={0.5}
-          readOnly
-        />
-      </ListItem>
+        </Grid>
+        <Grid item xs={12} sm={7} md={7} lg={7} xl={7}>
+          <ListItemText
+            primary={user}
+            secondary={
+              <>
+                <Typography
+                  sx={{ display: "inline", width: "100%" }}
+                  component="span"
+                  variant="body2"
+                  color="text.primary"
+                ></Typography>
+                {comment}
+              </>
+            }
+          />
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          sm={3}
+          md={3}
+          lg={3}
+          xl={3}
+          sx={{ display: "flex", justifyContent: "center" }}
+        >
+          <Rating
+            name="read-only"
+            value={rating}
+            size="small"
+            precision={0.5}
+            readOnly
+          />
+        </Grid>
+      </Grid>
       <Divider variant="inset" component="li" />
-    </>
+    </List>
   );
 };
 
