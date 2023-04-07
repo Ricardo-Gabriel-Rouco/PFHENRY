1748a117deeb3ac31c98eea44e42fba066dada2c
MLSalerno <mlslssl1991@gmail.com>
2023-04-06 22:35:12 -0300

    ..

--- client/src/Views/Home.jsx
+++ client/src/Views/Home.jsx
@@ -1,30 +1,31 @@
 import CardContainer from "../components/CardContainer/CardContainer";
-import {db} from "../firebase/firebase-config";
+import { db } from "../firebase/firebase-config";
 import { collection } from "firebase/firestore";
 import { getDocs } from "firebase/firestore";
 import { useDispatch } from "react-redux";
 import { addBook } from "../redux/rootReducer/bookSlice";
-import { useEffect} from "react";
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 
 
-const Home = ({cartOpen}) => {
+const Home = ({ cartOpen }) => {
   const dispatch = useDispatch();
 
   const bookCollectionRef = collection(db, "books");
-  // const query = new URLSearchParams(window.location.search);
-  // const data = JSON.parse(query.get('data'));
-  const {data} = useParams()
+  const querystring = window.location.search;
+  const params = new URLSearchParams(querystring);
+  const data = params.get('data');
+  const dataObj = JSON.parse(data);
 
   useEffect(() => {
     const getBooksList = async () => {
       try {
         const data = await getDocs(bookCollectionRef);
         const filterData = data.docs.map((books) => ({ ...books.data(), id: books.id }));
-        
+
         dispatch(addBook(filterData));
-      } catch (error) {}
+      } catch (error) { }
     };
 
     getBooksList();
@@ -35,8 +36,8 @@ const Home = ({cartOpen}) => {
 
   return (
     <>
-    {console.log(data)}
-      <CardContainer cartOpen={cartOpen}/>
+      {console.log(dataObj)}
+      <CardContainer cartOpen={cartOpen} />
     </>
   );
 };
