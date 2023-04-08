44126d0494f8f31e2bf5bc1a4171529330387e7c
MLSalerno <mlslssl1991@gmail.com>
2023-04-08 12:39:55 -0300

    .

--- api/src/controlers/payStatusControllers.js
+++ api/src/controlers/payStatusControllers.js
@@ -1,8 +1,9 @@
 const { mercadopago } = require('../mpConfig/mpConfig')
+const axios = require('axios')
 
 const verifyIdPayment = async function (id) {
     try {
-        const payment = await mercadopago.payment.get(id);
+        const payment = await axios.get(`https://api.mercadopago.com/v1/payments/${id}`);
         // if (payment.status === 'approved') {
         //     // Actualiza el estado del pago en la base de datos
         //     // y realiza cualquier otra acción necesaria
