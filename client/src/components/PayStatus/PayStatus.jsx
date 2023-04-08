8601c7f6ba26cf280414e74bab161d3158483687
MLSalerno <mlslssl1991@gmail.com>
2023-04-07 21:38:40 -0300

    ..

--- client/src/components/PayStatus/PayStatus.jsx
+++ client/src/components/PayStatus/PayStatus.jsx
@@ -9,26 +9,39 @@ const PayStatus = () => {
   const params = new URLSearchParams(querystring);
   const data2 = params.get('data');
   const dataObj = JSON.parse(data2);
+  const [status, setStatus] = useState("")
 
   useEffect(() => {
     // Elimina los productos del carrito al montar el componente
-    async function checkPayStatus(){
-        const response = await axios.post("https://pfhenry-production.up.railway.app/payStatus",
+    async function checkPayStatus() {
+      const response = await axios.post("https://pfhenry-production.up.railway.app/payStatus",
         dataObj.payment_id)
-        return response;
+      return response;
     }
     const payStatus = checkPayStatus()
-    if(payStatus){
-        dispatch(removeAllProducts());
-        localStorage.removeItem("cart");
+    if (payStatus) {
+      setStatus(payStatus)
+      dispatch(removeAllProducts());
+      localStorage.removeItem("cart");
     }
-    
+    else{
+      setStatus(payStatus)
+    }
   }, []);
 
   return (
     <div>
-      <h1>Compra Exitosa</h1>
-      <p>¡Gracias por su compra!</p>
+      {status === "approved" ? (
+        <div>
+          <h1>Compra Exitosa</h1>
+          <p>¡Gracias por su compra!</p>
+        </div>
+      ) : (
+        <div>
+          <h1>No se pudo realizar la compra</h1>
+          <p>Serás redirigido a Home</p>
+        </div>
+      )}
     </div>
   );
 };
