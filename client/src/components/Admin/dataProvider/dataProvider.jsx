b7f6e766d78826c0489f26e9cd5dd457400e7fb9
jgiampe <jggiampe@gmail.com>
2023-04-08 19:35:14 -0300

    Se corrigen filtros de Generos y Autores

--- client/src/components/Admin/dataProvider/dataProvider.jsx
+++ client/src/components/Admin/dataProvider/dataProvider.jsx
@@ -1,10 +1,9 @@
-
-import { getAllTheBooks, modifyBook, getBookById } from "../../../firebase/firestore/books";
+import {
+  getAllTheBooks,
+  modifyBook,
+  getBookById,
+} from "../../../firebase/firestore/books";
 import { getAllTheUsers } from "../../../firebase/firestore/users";
-
-
-
-
 
 const dataProvider = {
   getList: async (resource, params) => {
@@ -12,57 +11,66 @@ const dataProvider = {
     if (resource === "books") {
       const { title, genre, author } = params.filter || {}; // extrae los valores del filtro si se proporcionan
       const books = await getAllTheBooks();
-      data = books.map(el=>{return {
-        ...el,
-        authors: el.authors?.map(a=>{return {author:a}}),
-        genres: el.genres?.map(g=>{return {genre:g}})
-      }})
-      
-      data = data.filter((book) => {
-        // filtra los libros según los valores del filtro
-        if (title && !book.title.toLowerCase().includes(title.toLowerCase())) {
-          return false;
-        }
-        if (genre && !book.genres.map(g => g.toLowerCase()).includes(genre.toLowerCase())) {
-          return false
-        }
-        if (author && !book.authors.map(a=>a.toLowerCase()).includes(author.toLowerCase())){
-          return false
-        }
-        return true;
+      data = books.map((el) => {
+        return {
+          ...el,
+          authors: el.authors?.map((a) => {
+            return { author: a };
+          }),
+          genres: el.genres?.map((g) => {
+            return { genre: g };
+          }),
+        };
       });
+      
+      // Filtra los libros según los valores del filtro
+      if (title)
+        data = data.filter((book) => {
+          if (book.title.toLowerCase().includes(title.toLowerCase())) 
+            return true;
+        })
+      if(author)
+        data = data.filter((book) => {
+          if (book.authors.map((a) => a.author.toLowerCase()).some(item => item.includes(author.toLowerCase())))
+            return true;
+        });
+      if(genre)
+        data = data.filter((book) => {
+          if (book.genres.map((g) => g.genre.toLowerCase()).some(item => item.includes(genre.toLowerCase()))) 
+            return true
+        })
+
     } else if (resource === "users") {
       data = await getAllTheUsers();
     }
     return {
       data: data,
       total: data.length,
-    
     };
   },
-  getOne: async (resource,params) => {
+  getOne: async (resource, params) => {
     let data = {};
     const { id } = params;
-    console.log(params)
+    console.log(params);
 
     try {
       if (resource === "books") {
         data = await getBookById(id);
-      }
-      else{
+      } else {
         //get user by ID
         //data.data = await getUserById();
       }
-        console.log(data)
-        return {data}
+      console.log(data);
+      return { data };
     } catch (error) {
       console.log("Error en el servidor");
     }
   },
-  update: async (resource,params) =>{
-    if(resource === "books"){
-      const {id,data} = params;
-      await modifyBook(id,
+  update: async (resource, params) => {
+    if (resource === "books") {
+      const { id, data } = params;
+      await modifyBook(
+        id,
         data.authors,
         data.editorial,
         data.genres,
@@ -70,10 +78,14 @@ const dataProvider = {
         data.price,
         data.rating,
         data.title,
-        data.year)
-      console.log(`Book with ID ${id} has been modified with the following data: `, data);
-      return {data:{id:id,...data}}
+        data.year
+      );
+      console.log(
+        `Book with ID ${id} has been modified with the following data: `,
+        data
+      );
+      return { data: { id: id, ...data } };
     }
-  }
+  },
 };
 export default dataProvider;
