cc6aa0928eb8684be976519a60395ec41bfb3ed2
MLSalerno <mlslssl1991@gmail.com>
2023-04-08 23:40:14 -0300

    agregada propiedad email cuando se crea un user nuevo

--- client/src/firebase/firestore/users.js
+++ client/src/firebase/firestore/users.js
@@ -18,5 +18,21 @@ export async function getAllTheUsers() {
   return data
 }
 
+export async function getMailOfUser(id) {
+
+  const q = query(collection(db, "users"))
+  const querySnapshot = await getDocs(q);
+  let data = [];
+  querySnapshot.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    data.push({
+      ...doc.data(),
+      id: doc.id
+    }
+    )
+  })
+  return data
+}
+
 
 
