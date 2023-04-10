d9bde2ad3d2832e38985d8dcc586dbd0ad64af12
MLSalerno <mlslssl1991@gmail.com>
2023-04-10 18:19:49 -0300

    .

--- api/src/controlers/payStatusControllers.js
+++ api/src/controlers/payStatusControllers.js
@@ -2,11 +2,11 @@ const { mercadopago } = require('../mpConfig/mpConfig')
 
 const verifyIdPayment = async function (id) {
     try {
-        const payment = await mercadopago.payment.get(id);
-        if (payment.response.status === 'approved') {
-            return "approved";
-        }
-        return "failure";
+        // const payment = await mercadopago.payment.get(id);
+        // if (payment.response.status === 'approved') {
+        //     return "approved";
+        // }
+        return id;
     } catch (error) {
         return error
     }
