4fcdf8b6b89a71ced63d217df0e0f291dd45a8bd
jgiampe <jggiampe@gmail.com>
2023-04-11 20:32:00 -0300

    first autocomplete

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,70 +1,101 @@
-import style from './SearchBar.module.css';
-import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux'
+import style from "./SearchBar.module.css";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 // eslint-disable-next-line
-import { searchBook, clearSearchResults } from '../../redux/rootReducer/bookSlice';
-import SearchIcon from '@mui/icons-material/Search';
+import {
+  searchBook,
+  clearSearchResults,
+} from "../../redux/rootReducer/bookSlice";
+import SearchIcon from "@mui/icons-material/Search";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import { IconButton, InputBase, Paper } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
-import {IconButton, InputBase, Paper} from '@mui/material';
-import { useNavigate } from 'react-router-dom';
+const SearchBar = ({ placeholder }) => {
+  // eslint-disable-next-line
+  const books = useSelector((state) => state.books.displayableBooks);
 
+  // console.log(books)
 
+  //ESTADO PARA SETEAR LOS VALORES DE BUSQUEDA
+  const [searchValue, setSearchValue] = useState("");
+  //ESTADO PARA LOS RESULTADOS DE LA BUSQUEDA
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  // HANDLER DEL INPUT CHANGE TOMA EL VALOR PARA COMPARAR
+  const handlerInputChange = (e) => {
+    setSearchValue(e.target.value);
+  };
 
+  //HANDLER DEL BOTON DE BUSQUEDA
 
-const SearchBar = ({
-    placeholder
-
-}) => {
-    // eslint-disable-next-line
-    const books = useSelector((state) => state.books.booksToFilter)
-
-    // console.log(books)
-
-    //ESTADO PARA SETEAR LOS VALORES DE BUSQUEDA
-    const [searchValue, setSearchValue] = useState("");
-    //ESTADO PARA LOS RESULTADOS DE LA BUSQUEDA
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    // HANDLER DEL INPUT CHANGE TOMA EL VALOR PARA COMPARAR
-    const handlerInputChange = (e) => {
-        setSearchValue(e.target.value)
+  const handlerSearchClick = () => {
+    dispatch(searchBook(searchValue));
+    setSearchValue("");
+    navigate("/home");
+  };
+  //ENTER BUTTON
+  const handlerKeyDown = (e) => {
+    if (e.key === "Enter") {
+      handlerSearchClick();
     }
+  };
 
-    //HANDLER DEL BOTON DE BUSQUEDA
+  function filterOptions(options, { inputValue }) {
+    // Only show options that include the input value
+    return options.filter((option) =>
+      option.toLowerCase().includes(inputValue.toLowerCase())
+    ).slice(0, 5); // Limit the number of displayed options to 5
+  }
 
-    const handlerSearchClick = () =>{
-        dispatch(searchBook(searchValue))
-        setSearchValue("")
-        navigate('/home');
-    }
-    //ENTER BUTTON 
-    const handlerKeyDown = (e) => {
-        if (e.key === 'Enter') {
-            handlerSearchClick()
-        }
-    }
-
-    return (
-        <div className={style.SearchBarContainer}>
-            <div className={style.SearchBar}>
-                <Paper sx={{ display: 'flex', alignSelf: 'center', width: '80%' }} margin="dense">
-                    <InputBase
-                        type="text"
-                        margin='dense'
-                        placeholder={placeholder}
-                        value={searchValue}
-                        onChange={handlerInputChange}
-                        className={style.SearchInput}
-                        onKeyDown={handlerKeyDown}
-                        sx={{ ml: 1, flex: 1 }}
-                    />
-                    <IconButton type='button' sx={{ p: 'px' }} aria-label='search' onClick={handlerSearchClick}>
-                        <SearchIcon  />
-                    </IconButton>
-                </Paper>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className={style.SearchBarContainer}>
+      <div className={style.SearchBar}>
+        <Paper
+          sx={{ display: "flex", alignSelf: "center", width: "80%" }}
+          margin="dense"
+        >
+          <Autocomplete
+            freeSolo
+            id="free-solo-2-demo"
+            disableClearable
+            options={books.map((el) => el.title)}
+            filterOptions={filterOptions}
+            sx={{ ml: 1, flex: 1 }}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Search input"
+                InputProps={{
+                  ...params.InputProps,
+                  type: "search",
+                }}
+              />
+            )}
+          />
+          {/* <InputBase
+            type="text"
+            margin="dense"
+            placeholder={placeholder}
+            value={searchValue}
+            onChange={handlerInputChange}
+            className={style.SearchInput}
+            onKeyDown={handlerKeyDown}
+            sx={{ ml: 1, flex: 1 }}
+          /> */}
+          <IconButton
+            type="button"
+            sx={{ p: "px" }}
+            aria-label="search"
+            onClick={handlerSearchClick}
+          >
+            <SearchIcon />
+          </IconButton>
+        </Paper>
+      </div>
+    </div>
+  );
+};
 
 export default SearchBar;
