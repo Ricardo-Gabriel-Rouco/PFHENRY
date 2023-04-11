96f9e95efd24ca6fcec54b06ccc2195e262f180b
jgiampe <jggiampe@gmail.com>
2023-04-11 11:13:10 -0300

    Se corrige bug en cart

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -14,12 +14,12 @@ import { toogleCart } from "../../redux/rootReducer/toogleSlice";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useAuth } from "../../context/authContext";
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 import { postOrder } from "../../firebase/firestore/orders";
 
 const Cart = () => {
   const { userStatus } = useAuth();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const cart = useSelector((state) => state.cart);
   const abrir = useSelector((state) => state.toogle.isOpen);
@@ -33,14 +33,13 @@ const Cart = () => {
         name: userStatus.userId,
         email: userStatus.email,
       },
-      items: cart.cart.cart.map((product,i) => ({
+      items: cart.cart.cart.map((product, i) => ({
         quantity: product.quantity,
-        ...displayableBooks.find(el=>el.id===product.id)
+        ...displayableBooks.find((el) => el.id === product.id),
       })),
     });
     // eslint-disable-next-line
   }, [cart.cart.cart]);
-
 
   //handlers
 
@@ -53,15 +52,15 @@ const Cart = () => {
         );
         await postOrder(response.data);
         dispatch(removeAllProducts());
-        localStorage.removeItem("cart")
+        localStorage.removeItem("cart");
         window.open(response.data.link);
       } catch (error) {
         console.error("Error:", error.response.data);
       }
     } else {
       dispatch(toogleCart());
-      alert('You must be logged to buy')
-      navigate('/login')
+      alert("You must be logged to buy");
+      navigate("/login");
     }
   };
 
@@ -69,7 +68,8 @@ const Cart = () => {
     dispatch(toogleCart());
   };
   const handleAdd = (id) => {
-    dispatch(addProduct(id));
+    console.log(id);
+    dispatch(addProduct({ id }));
   };
 
   const handleRemove = (id) => {
@@ -100,48 +100,50 @@ const Cart = () => {
           </Button>
         </Stack>
         <Divider sx={{ my: 1.5 }} />
-        {cart.cart.cart.map((product,i) => ({
-        quantity: product.quantity,
-        ...displayableBooks.find(el=>el.id===product.id)
-      })).map((product) => {
-          return (
-            <Grid container spacing={2} key={product.id}>
-              <Grid item xs={4}>
-                <img src={product.image} alt={product.title} width="100%" />
+        {cart.cart.cart
+          .map((product, i) => ({
+            quantity: product.quantity,
+            ...displayableBooks.find((el) => el.id === product.id),
+          }))
+          .map((product) => {
+            return (
+              <Grid container spacing={2} key={product.id}>
+                <Grid item xs={4}>
+                  <img src={product.image} alt={product.title} width="100%" />
+                </Grid>
+                <Grid item xs={8}>
+                  <Typography variant="subtitle1">{product.title}</Typography>
+                  <Typography variant="subtitle2" color="text.secondary">
+                    ({product.authors})
+                  </Typography>
+                  <Typography variant="subtitle1" sx={{ fontWeight: "bold" }}>
+                    ${product.price}
+                  </Typography>
+                  <Box sx={{ display: "flex" }}>
+                    <Button
+                      onClick={() => handleRemove(product.id)}
+                      variant="contained"
+                      color="primary"
+                      size="small"
+                      sx={{ ml: 1 }}
+                    >
+                      <RemoveIcon />
+                    </Button>
+                    <Button
+                      onClick={() => handleAdd(product.id)}
+                      variant="contained"
+                      color="primary"
+                      size="small"
+                      sx={{ mr: 1 }}
+                    >
+                      <AddIcon />
+                    </Button>
+                    <Typography>{product.quantity}</Typography>
+                  </Box>
+                </Grid>
               </Grid>
-              <Grid item xs={8}>
-                <Typography variant="subtitle1">{product.title}</Typography>
-                <Typography variant="subtitle2" color="text.secondary">
-                  ({product.authors})
-                </Typography>
-                <Typography variant="subtitle1" sx={{ fontWeight: "bold" }}>
-                  ${product.price}
-                </Typography>
-                <Box sx={{ display: "flex" }}>
-                  <Button
-                    onClick={() => handleRemove(product.id)}
-                    variant="contained"
-                    color="primary"
-                    size="small"
-                    sx={{ ml: 1 }}
-                  >
-                    <RemoveIcon />
-                  </Button>
-                  <Button
-                    onClick={() => handleAdd(product.id)}
-                    variant="contained"
-                    color="primary"
-                    size="small"
-                    sx={{ mr: 1 }}
-                  >
-                    <AddIcon />
-                  </Button>
-                  <Typography>{product.quantity}</Typography>
-                </Box>
-              </Grid>
-            </Grid>
-          );
-        })}
+            );
+          })}
         {cart.cart.cart.length === 0 && (
           <Typography variant="subtitle1">
             There is no product in your cart
@@ -149,7 +151,13 @@ const Cart = () => {
         )}
         {cart.cart.cart.length !== 0 && (
           <Typography variant="subtitle1">
-            {"Total Price: $ " + cart.cart.totalPrice}
+            {"Total Price: $ " + cart.cart.cart
+          .map((product, i) => ({
+            quantity: product.quantity,
+            ...displayableBooks.find((el) => el.id === product.id),
+          }))
+          .reduce((totalPrice,book)=>totalPrice+(book.quantity*book.price),0)
+            }
           </Typography>
         )}
         {cart.cart.cart.length !== 0 ? (
@@ -197,7 +205,6 @@ const Cart = () => {
             </Button>
           </>
         )}
-
       </Box>
     </Drawer>
   );
