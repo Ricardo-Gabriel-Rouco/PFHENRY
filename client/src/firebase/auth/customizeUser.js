4be5170e627e1f78731f0af1e78fa62cc445e7aa
Gabriel Rouco <r.g.rouco1@gmail.com>
2023-04-10 23:22:00 -0300

    creada funcion del back para edicion de parametros de usuarios

---
+++ client/src/firebase/auth/customizeUser.js
@@ -0,0 +1,16 @@
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../firebase-config";
+
+export async function updateUser(userId, nickname, fullname){
+    const userRef = doc(db, 'users', userId)
+    try {
+        const updateObj = {}
+        if (nickname) updateObj.nickname = nickname
+        if (fullname) updateObj.fullname = fullname
+        
+        await updateDoc(userRef, updateObj)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  
