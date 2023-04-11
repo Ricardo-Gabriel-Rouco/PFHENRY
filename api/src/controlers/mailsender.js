d07d095d3057c7b3c240a6d52f20b48048ee9013
Gabriel Rouco <r.g.rouco1@gmail.com>
2023-04-10 21:25:38 -0300

    ...

--- api/src/controlers/mailsender.js
+++ api/src/controlers/mailsender.js
@@ -1,26 +1,32 @@
 const nodemailer = require('nodemailer')
 const template = require('./configmail')
-const { collection, onSnapshot} = require("firebase/firestore");
-const db = require('../../firebase-config')
+// const { collection, getDocs, query} = require("firebase/firestore");
+// const db = require('../../firebase-config')
 require('dotenv').config();
 const {MAILUSER, MAILPASSWORD} = process.env
 
 
-async function getConfig(){
-  let options = []
-  const data = collection(db, "mailConfig")
-  const results = await onSnapshot(data, (option)=> {
-    option.docs.map(doc => options.push(doc.data()))
-  })
-  return results
-}
+// async function getConfig(){
+//     const q = query(collection(db, "mailConfig"))
+//     const querySnapshot = await getDocs(q);
+//     let data = [];
+//     querySnapshot.forEach((doc) => {
+//       // doc.data() is never undefined for query doc snapshots
+//       data.push({
+//         ...doc.data(),
+//         id: doc.id
+//       }
+//       )
+//     })
+//     return data
+//   }
+
 
 async function sender(mail, reason){
   // let testAccount = await nodemailer.createTestAccount()
 
-  const configs = await getConfig()
+  // const configs = await getConfig()
 
-  console.log(configs)
   let transporter = nodemailer.createTransport({
     // host: 'smtp.ethereal.email',
     service: 'hotmail',
--- api/src/routes/mailRoute.js
+++ api/src/routes/mailRoute.js
@@ -2,6 +2,6 @@ const express = require("express");
 const { sendingEmail } = require("../handlers/mailHandler");
 const router = express.Router();
 
-router.get("/", sendingEmail)
+router.post("/", sendingEmail)
 
 module.exports = router;
