06cda09bb97e7122cff65d8837f7408c129fab42
MLSalerno <mlslssl1991@gmail.com>
2023-04-12 15:27:39 -0300

    .

--- api/src/mpConfig/mpConfig.js
+++ api/src/mpConfig/mpConfig.js
@@ -1,7 +1,7 @@
 const mercadopago = require('mercadopago');
 
 mercadopago.configure({
-    access_token: 'TEST-3594846635662107-041019-cde2acfe8adc065118f43fd63a8794af-1349625519'
+    access_token: 'APP_USR-3594846635662107-041019-7bd48c43894ac0e2bd16975b12b28dbc-1349625519'
 });
 
 module.exports = {
