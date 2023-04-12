f7e69b442c746c42928db2b9428c401382817749
Joaquin Santiago Oliveira <104986417+fr0st1987@users.noreply.github.com>
2023-04-12 19:08:06 -0300

    Agrego 12 libros random, con descunetos, al carrousel

--- client/src/components/Carrousel/Carrousel.jsx
+++ client/src/components/Carrousel/Carrousel.jsx
@@ -3,11 +3,16 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import SwiperCore, { Navigation, Pagination } from "swiper/core";
-import { CardMedia, Grid, Typography,styled } from "@mui/material";
+import { CardMedia, Grid, Typography, styled } from "@mui/material";
+import { getDocs, query, collection, where, doc, getDoc, updateDoc, setDoc, arrayUnion } from "firebase/firestore";
+import { db } from '../../firebase/firebase-config';
+import { useState } from "react";
+import { useEffect } from "react";
+import { applyDiscounts } from '../../firebase/firestore/discount'
 
 SwiperCore.use([Navigation, Pagination]);
 
-const Carrousel = ({ currentBook }) => {
+const Carrousel = () => {
     const CarouselContainer = styled('div')({
         backgroundColor: "#f9b52ea8",
         margin: 'auto',
@@ -20,30 +25,55 @@ const Carrousel = ({ currentBook }) => {
         height: "14rem",
         objectFit: "cover",
     });
+    const [discounts, setDiscounts] = useState([]);
+
+
+    useEffect(() => {
+        const fetchDiscounts = async () => {
+            const q = collection(db, 'discount');
+            const querySnapshot = await getDocs(q);
+            const data = [];
+            querySnapshot.forEach((doc) => {
+                data.push({
+                    ...doc.data(),
+                    id: doc.id,
+                });
+            });
+            setDiscounts(data);
+        };
+        fetchDiscounts()
+        
+    }, []);
+
+    console.log(discounts)
 
     return (
         <CarouselContainer>
             <Swiper
-                slidesPerView={1}
+                slidesPerView={4}
+                slidesPerGroup={4}
                 spaceBetween={100}
                 navigation
                 pagination={{ clickable: true }}
                 breakpoints={{
                     640: {
                         slidesPerView: 2,
+                        slidesPerGroup: 2,
                         spaceBetween: 20,
                     },
                     768: {
                         slidesPerView: 3,
+                        slidesPerGroup: 3,
                         spaceBetween: 30,
                     },
                     1024: {
                         slidesPerView: 4,
+                        slidesPerGroup: 4,
                         spaceBetween: 10,
                     },
                 }}
             >
-                {currentBook.map((book) => (
+                {discounts.map((book) => (
                     <SwiperSlide key={book.id}>
                         <Grid container justifyContent="center" alignItems="center" style={{ height: '100%' }}>
                             <Grid item>
@@ -52,7 +82,7 @@ const Carrousel = ({ currentBook }) => {
                                     height="300"
                                     image={book.image}
                                     alt={book.title} />
-                               <Typography align="center" variant="subtitle2">{book.title}</Typography>
+                                <Typography align="center" variant="subtitle2">{book.title}</Typography>
                             </Grid>
                         </Grid>
                     </SwiperSlide>
--- client/src/firebase/firestore/discount.js
+++ client/src/firebase/firestore/discount.js
@@ -1,12 +1,15 @@
-import { addDoc, collection, query, where, getDocs } from "firebase/firestore";
+import { addDoc, collection, query, where, getDocs, setDoc, doc, deleteDoc } from "firebase/firestore";
 import { db } from '../firebase-config';
 
 export const applyDiscounts = async () => {
-    const originalDB = query(collection(db, "books"), where('display', '==', true))
+    const originalDB = query(collection(db, "books"), where('display', '==', true));
     const discountDB = collection(db, 'discount');
 
-    const querySnapshot = await getDocs(originalDB)
-
+    const querySnapshot = await getDocs(originalDB);
+    const querySnapshot2 = await getDocs(collection(db, 'discount'));
+    querySnapshot2.forEach((doc) => {
+        deleteDoc(doc.ref);
+    });
     const books = [];
     querySnapshot.forEach((doc) => {
         // doc.data() is never undefined for query doc snapshots
@@ -17,19 +20,27 @@ export const applyDiscounts = async () => {
     });
 
     const descuentoIndexes = [];
-    for (let i = 0; i < books.length; i++) {
-        if (Math.random() < 0.2) { // 20% de los libros
+    for (let i = 0; i < books.length && descuentoIndexes.length <= 11; i++) {
+        if (Math.random() < 0.5) { // 50% de los libros
             descuentoIndexes.push(i);
         }
     }
 
+    const discountDocs = await getDocs(collection(db, 'discount'));
+    const existingDiscounts = discountDocs.docs.map((doc) => doc.data());
+
     descuentoIndexes.forEach(index => {
         const book = books[index];
         const discount = 0.15; // Descuento del 15%
-        const newPrice = books.unite_price * (1 - discount);
-        const newDiscountDoc = { ...book, unite_price: newPrice };
-        addDoc(discountDB, newDiscountDoc); // Utilizamos addDoc() para agregar el documento
+        const newPrice = book.price * (1 - discount);
+        const existingDiscount = existingDiscounts.find((discountDoc) => discountDoc.bookId === book.id);
+        if (existingDiscount) {
+            setDoc(doc(discountDB, existingDiscount.id), { ...existingDiscount, price: newPrice });
+        } else {
+            const newDiscountDoc = { ...book, price: newPrice, bookId: book.id };
+            addDoc(discountDB, newDiscountDoc);
+        }
     });
 
     return null;
-};
+}
