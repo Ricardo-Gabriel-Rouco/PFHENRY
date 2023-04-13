0ef376e75a5af6188193e630318bac54944814c8
Gabriel Rouco <r.g.rouco1@gmail.com>
2023-04-13 20:46:08 -0300

    seguimos probando cors

--- api/src/app.js
+++ api/src/app.js
@@ -14,10 +14,17 @@ const app = express();
 //middlewares
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(cors({
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
-  origin: ['http://localhost:3000', 'https://pfhenry-jzy1.vercel.app', 'https://pfhenry-production.up.railway.app/payStatus', 'https://pfhenry-jzy1-ds8p7673k-ricardo-gabriel-rouco.vercel.app', 'http://www.mercadopago.com.ar'] // Allow requests from these origins only
-}));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000', 'https://pfhenry-jzy1.vercel.app', 'https://pfhenry-production.up.railway.app/payStatus', 'https://pfhenry-jzy1-ds8p7673k-ricardo-gabriel-rouco.vercel.app', 'http://www.mercadopago.com.ar'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+// app.use(cors({
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+//   origin: ['http://localhost:3000', 'https://pfhenry-jzy1.vercel.app', 'https://pfhenry-production.up.railway.app/payStatus', 'https://pfhenry-jzy1-ds8p7673k-ricardo-gabriel-rouco.vercel.app', 'http://www.mercadopago.com.ar'] // Allow requests from these origins only
+// }));
 app.use("/clients", clients);
 app.use("/products", products);
 app.use("/checkout", payChoosen);
