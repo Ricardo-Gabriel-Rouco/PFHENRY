a33338b214eaf0af7f58335adef6da66de19b47f
Joaquin Santiago Oliveira <104986417+fr0st1987@users.noreply.github.com>
2023-04-14 13:22:09 -0300

    Se le agregan algo de estilos, falta ver tema CardDetail

--- client/src/components/Carrousel/Carrousel.jsx
+++ client/src/components/Carrousel/Carrousel.jsx
@@ -5,10 +5,20 @@ import 'swiper/css/pagination';
 import SwiperCore, { Navigation, Pagination } from "swiper/core";
 import { CardMedia, Grid, Typography, styled } from "@mui/material";
 import { useSelector } from "react-redux";
+import { useState } from "react";
+import CardDetail from '../CardDetail/CardDetail'
+import './Carrousel.css'
+
 
 SwiperCore.use([Navigation, Pagination]);
 
 const Carrousel = () => {
+    const displayableBooks = useSelector((state) => state.books.displayableBooks);
+    const [selectedBookId, setSelectedBookId] = useState(null);
+
+    const handleBookClick = (bookId) => {
+        setSelectedBookId(bookId);
+    }
     const CarouselContainer = styled('div')({
         backgroundColor: "#f9b52ea8",
         margin: 'auto',
@@ -24,11 +34,15 @@ const Carrousel = () => {
         zIndex: 0
     });
 
-    const displayableBooks = useSelector((state) => state.books.displayableBooks);
+    
+
 
 
     return (
         <CarouselContainer>
+            <Typography align="left" variant="h6" marginLeft={'15px'}>
+                {'Ofertas'}
+            </Typography>
             <Swiper
                 slidesPerView={4}
                 slidesPerGroup={4}
@@ -61,22 +75,38 @@ const Carrousel = () => {
                 }}
             >
                 {displayableBooks.map((book) => (
-                    book.display && book.discount?
+                    book.display && book.discount ?
                         <SwiperSlide key={book.id}>
-                            <Grid container justifyContent="center" alignItems="center" style={{ height: '100%' }}>
+                            {console.log(book.id)}
+                            <Grid container justifyContent="center" style={{ height: '100%' }}>
                                 <Grid item>
                                     <BookCardMedia
                                         component="img"
                                         height="300"
                                         image={book.image}
-                                        alt={book.title} />
-                                    <Typography align="center" variant="subtitle2">{book.title}</Typography>
+                                        alt={book.title}
+                                        sx={{ cursor: 'pointer' }}
+                                        onClick={() => handleBookClick(book.id)} />
+                                    {selectedBookId === book.id && <CardDetail id={book.id} />}
+                                    <Grid container justifyContent="space-between" alignItems="center">
+                                        <Typography align="left" variant="h7" sx={{ textAlign: 'left' }}>
+                                            $<s>{book.price}</s>{" "}
+                                        </Typography>
+                                        <Typography align="left" variant="h7" fontWeight={'bold'} sx={{ textAlign: 'left' }}>
+                                            <span>
+                                                ${(book.price * (100 - book.discount) / 100).toFixed(2)}
+                                            </span>
+                                        </Typography>
+                                    </Grid>
+                                    <Typography align="right" variant="h7" sx={{ textAlign: 'right', color: 'brown', }}>
+                                        %{book.discount} {' off'}
+                                    </Typography>
                                 </Grid>
                             </Grid>
                         </SwiperSlide>
                         : null))}
             </Swiper>
-        </CarouselContainer>
+        </CarouselContainer >
     );
 };
 
