3b8255b23edaaafd24316a49f7d1e1b2d34cecc1
Gabriel Rouco <r.g.rouco1@gmail.com>
2023-04-13 21:10:22 -0300

    .

--- api/src/app.js
+++ api/src/app.js
@@ -31,21 +31,40 @@ app.use(morgan("dev"));
 //   credentials: true, // Permitir solicitudes con credenciales (como cookies o encabezados de autorización)
 //   allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'], // Permitir los encabezados personalizados Origin, X-Requested-With, Content-Type y Accept
 // }));
-app.use(cors({
-  origin: ['http://localhost:3000', 'https://pfhenry-jzy1.vercel.app', 'https://pfhenry-production.up.railway.app/payStatus', 'https://pfhenry-jzy1-ds8p7673k-ricardo-gabriel-rouco.vercel.app', 'http://www.mercadopago.com.ar'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
-  credentials: true,
-  preflightContinue: true, // Permitir que se continúe la solicitud preflight OPTIONS
-}));
+// app.use(cors({
+//   origin: ['http://localhost:3000', 'https://pfhenry-jzy1.vercel.app'],
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
+//   credentials: true,
+//   preflightContinue: true, // Permitir que se continúe la solicitud preflight OPTIONS
+// }));
 
-app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', 'https://pfhenry-jzy1.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.send();
-});
+// app.options('*', (req, res) => {
+//   res.header('Access-Control-Allow-Origin', 'https://pfhenry-jzy1.vercel.app');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   res.send();
+// });
+
+module.exports = (req, res) => {
+  //set header first to allow request or origin domain (value can be different)
+  res.setHeader('Access-Control-Allow-Origin', ['http://localhost:3000', 'https://pfhenry-jzy1.vercel.app']);
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, OPTIONS, DELETE');
+
+//---- other code
+
+//Preflight CORS handler
+  if(req.method === 'OPTIONS') {
+      return res.status(200).json(({
+          body: "OK"
+      }))
+  }
+
+}
+
 app.use("/clients", clients);
 app.use("/products", products);
 app.use("/checkout", payChoosen);
