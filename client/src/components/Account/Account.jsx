3e65627c5decd1d945021e0adf5a9c3dde91e499
Joaquin Santiago Oliveira <104986417+fr0st1987@users.noreply.github.com>
2023-04-16 16:21:47 -0300

    new view my books

--- client/src/components/Account/Account.jsx
+++ client/src/components/Account/Account.jsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import { useAuth } from "../../context/authContext";
 import { getOrdersByUser } from "../../firebase/firestore/orders";
-import { Grid, Paper, Typography, CardMedia, TableCell, TableRow, TableHead, Table, TableContainer, TableBody } from '@mui/material';
+import { Grid, GridContainer, Paper, Typography, CardMedia, TableCell, TableRow, TableHead, Table, TableContainer } from '@mui/material';
 
 
 
@@ -19,10 +19,9 @@ function Account() {
         fetchOrders();
     }, [userStatus.userId]);
 
-
     return (
         <TableContainer component={Paper}>
-            <Table sx={{ width: '100%', alignContent: 'center' }} aria-label="orders table">
+            <Table sx={{ width: '100%', alignContent: 'center', boxShadow: '1px 1px 3px rgba(0, 0, 0, 0.3)' }} aria-label="orders table">
                 {orders.map((order) => (<>
                     <TableHead>
                         <TableCell align="left">{new Date(order.date).toLocaleDateString([], { day: 'numeric', month: 'long', year: 'numeric' })}</TableCell>
@@ -30,25 +29,33 @@ function Account() {
                     <TableRow key={order.id}>
                         <TableCell colSpan={3}>
 
-                            {order.items.map((book) => (
-                                <Grid item xs={12} sm={6} md={4} key={book.id}>
-                                    <Paper sx={{ display: 'flex', alignItems: 'center', padding: 2 }}>
-                                        <CardMedia
-                                            component="img"
-                                            image={book.image}
-                                            sx={{ width: 50, height: 50, mr: 2 }}
-                                        />
-                                        <div>
-                                            <Typography variant="subtitle1" gutterBottom>
-                                                {book.title}
-                                            </Typography>
-                                            <Typography variant="body2" color="textSecondary">
-                                                ${book.price}
-                                            </Typography>
-                                        </div>
-                                    </Paper>
-                                </Grid>
-                            ))}
+                            <Grid container spacing={2}>
+                                {order.items.map((book) => (
+                                    <Grid item xs={12} sm={6} md={4} key={book.id}>
+
+                                        <Paper sx={{ display: 'flex', alignItems: 'center', padding: 2 }}>
+                                            <CardMedia
+                                                component="img"
+                                                border='1px solid'
+                                                borderRadius="10px"
+                                                image={book.image}
+                                                sx={{ width: 90, height: 90, mr: 2, boxShadow: '1px 1px 3px rgba(0, 0, 0, 0.3)' }}
+                                            />
+                                            <div>
+                                                <Typography fontWeight='bold' variant="subtitle1" gutterBottom>
+                                                    {book.title}
+                                                </Typography>
+                                                <Typography variant="body2" color="textSecondary">
+                                                    ${book.price}
+                                                    <br />
+                                                    Quantity: {book.quantity}
+                                                </Typography>
+                                            </div>
+                                        </Paper>
+                                    </Grid>
+
+                                ))}
+                            </Grid>
 
                         </TableCell>
                     </TableRow></>
