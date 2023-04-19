4dc6df39ea578c0b71d011cb3d1da48aa4a34948
mmitacc <manuelmitacc@hotmail.com>
2023-04-19 14:42:49 -0500

    fixed Landing.jsx & style

--- client/src/Views/Landing/Landing.jsx
+++ client/src/Views/Landing/Landing.jsx
@@ -1,28 +1,17 @@
 import { useEffect, useState } from "react";
 import { Button, Grid, Typography, CardMedia } from "@mui/material";
-import { keyframes } from "@emotion/react";
 
-const opacities = keyframes`
-  0% {
-    opacity: 1,
-    transform: "scale(1)",
-  },
-  100% {
-    opacity: 0,
-    transform: "scale(2)",
-  },
-`;
 const Landing = () => {
   const allImage = 5; //define el numero de "image" a renderizar
   const [selectImage, SetSelectImage] = useState(0);
-  const [loading, SetLoading] = useState(false); //controlando el "loading" de carga de image
+  const [loaded, SetLoaded] = useState(false); //controlando el "loaded" de carga de imagen
   useEffect(() => {
     const interval = setInterval(() => {
+      SetLoaded(false);
       selectImage < allImage - 1
         ? SetSelectImage(selectImage + 1)
         : SetSelectImage(0);
-    }, 2000);
-    SetLoading(false);
+    }, 2500);
     return () => clearInterval(interval);
   });
 
@@ -57,19 +46,104 @@ const Landing = () => {
       <Grid item xs="12" sx={{ margin: "auto" }}>
         <CardMedia
           component="img"
-          onLoad={() => SetLoading(true)}
-          image={require(`../../Assets/imgLanding/image${selectImage}.jpg`)}
+          src={require(`../../Assets/imgLanding/image${selectImage}.jpg`)}
+          onLoad={() => SetLoaded(true)}
           alt="The way to Book's Kingdom"
+          {...(loaded ? { timeout: 2000 } : {})}
           style={{
-            animation: `${opacities} 2s infinite ease`,
+            transition: loaded ? "1000ms" : undefined,
+            opacity: loaded ? 1 : 0,
             borderRadius: "30px",
             border: "solid black 0.1rem",
-            width: "60%",
+            minWidth: 500,
+            width: "50%",
             margin: "auto",
           }}
         />
       </Grid>
+      <Grid container position="relative">
+        <Grid item xs="auto" sx={{ bgcolor: "#000000" }}>
+          <CardMedia
+            component="img"
+            src={require(`../../Assets/imgLanding/image0.jpg`)}
+            alt="The way to Book's Kingdom"
+            {...(loaded && selectImage === 0 ? { timeout: 2000 } : {})}
+            style={{
+              transition: loaded && selectImage === 0 ? "1000ms" : undefined,
+              opacity: loaded && selectImage === 0 ? 1 : 0.3,
+              border: "solid black 0.1rem",
+              minWidth: 100,
+              width: "10%",
+              margin: "auto",
+            }}
+          />
+        </Grid>
 
+        <Grid item xs="auto" sx={{ bgcolor: "#000000" }}>
+          <CardMedia
+            component="img"
+            src={require(`../../Assets/imgLanding/image1.jpg`)}
+            alt="The way to Book's Kingdom"
+            {...(loaded && selectImage === 1 ? { timeout: 2000 } : {})}
+            style={{
+              transition: loaded && selectImage === 1 ? "1000ms" : undefined,
+              opacity: loaded && selectImage === 1 ? 1 : 0.3,
+              border: "solid black 0.1rem",
+              minWidth: 100,
+              width: "10%",
+              margin: "auto",
+            }}
+          />
+        </Grid>
+        <Grid item xs="auto" sx={{ bgcolor: "#000000" }}>
+          <CardMedia
+            component="img"
+            src={require(`../../Assets/imgLanding/image2.jpg`)}
+            alt="The way to Book's Kingdom"
+            {...(loaded && selectImage === 2 ? { timeout: 2000 } : {})}
+            style={{
+              transition: loaded && selectImage === 2 ? "1000ms" : undefined,
+              opacity: loaded && selectImage === 2 ? 1 : 0.3,
+              border: "solid black 0.1rem",
+              minWidth: 100,
+              width: "10%",
+              margin: "auto",
+            }}
+          />
+        </Grid>
+        <Grid item xs="auto" sx={{ bgcolor: "#000000" }}>
+          <CardMedia
+            component="img"
+            src={require(`../../Assets/imgLanding/image3.jpg`)}
+            alt="The way to Book's Kingdom"
+            {...(loaded && selectImage === 3 ? { timeout: 2000 } : {})}
+            style={{
+              transition: loaded && selectImage === 3 ? "1000ms" : undefined,
+              opacity: loaded && selectImage === 3 ? 1 : 0.3,
+              border: "solid black 0.1rem",
+              minWidth: 100,
+              width: "10%",
+              margin: "auto",
+            }}
+          />
+        </Grid>
+        <Grid item xs="auto" sx={{ bgcolor: "#000000" }}>
+          <CardMedia
+            component="img"
+            src={require(`../../Assets/imgLanding/image4.jpg`)}
+            alt="The way to Book's Kingdom"
+            {...(loaded && selectImage === 4 ? { timeout: 2000 } : {})}
+            style={{
+              transition: loaded && selectImage === 4 ? "1000ms" : undefined,
+              opacity: loaded && selectImage === 4 ? 1 : 0.3,
+              border: "solid black 0.1rem",
+              minWidth: 100,
+              width: "10%",
+              margin: "auto",
+            }}
+          />
+        </Grid>
+      </Grid>
       <Grid item xs="12">
         <Button
           variant="contained"
